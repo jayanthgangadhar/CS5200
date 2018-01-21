9ad32258f272c2ab18e17660db9826e5513bd757
Jayanth Gangadhar <jayanthgangadhar@Apples-MacBook-Pro.local>
2018-01-21 14:44:14 -0500

    First commit

---
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -0,0 +1,16 @@
+package edu.northeastern.cs5200.controllers.hello;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+	@RequestMapping("/api/hello/string")
+	public String sayHello() {
+		return "Hello Jayanth Gangadhar!";
+	}
+	@RequestMapping("/api/hello/object")
+	public HelloObject sayHelloObject() {
+		HelloObject obj = new HelloObject("Hello Jayanth Gangadhar!");
+		return obj;
+	}
+
+}
